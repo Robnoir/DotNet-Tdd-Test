b2382694915a9d5f897c128de96b4c76f1720e25
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2023-11-16 17:51:29 +0100

    ändrade switch case metoden så den har en boolean

--- FizzBuzz/Kalkylator.cs
+++ FizzBuzz/Kalkylator.cs
@@ -17,39 +17,25 @@ namespace FizzBuzz
                 throw new ArgumentOutOfRangeException(nameof(nummer), "nummer kan ej va negativt");
             }
 
+            bool divisibleBy3 = nummer % 3 == 0;
+            bool divisibleBy5 = nummer % 5 == 0;
+
             switch (nummer)
             {
-                case int n when (n % 3 == 0 && n % 5 == 0):
+                case int n when (divisibleBy3 && divisibleBy5):
                     return "FizzBuzz";
-                case int n when (n % 3 == 0):
+                case int n when (divisibleBy3):
                     return "Fizz";
-                case int n when (n % 5 == 0):
+                case int n when (divisibleBy5):
                     return "Buzz";
                 default:
                     return nummer.ToString();
             }
         }
 
-        //public static string FizzBuzzKalkyl(int nummer)
-        //{
-        //    if (nummer < 0)
-        //    {
-        //        throw new ArgumentOutOfRangeException(nameof(nummer), "nummer kan ej va negativt");
-        //    }
-
-        //    bool divisibleBy3 = nummer % 3 == 0;
-        //    bool divisibleBy5 = nummer % 5 == 0;
-
-        //    if (divisibleBy3 && divisibleBy5) return "FizzBuzz";
-        //    if (divisibleBy3) return "Fizz";
-        //    if (divisibleBy5) return "Buzz";
-
-        //    return nummer.ToString();
-        //}
-
         public static void HanteraOchSkrivUtAnvändarInput()
         {
-            var input = LäsInput();
+            string input = LäsInput();
             if (ÄrExitKommando(input))
                 return;
 
@@ -60,12 +46,12 @@ namespace FizzBuzz
         private static string LäsInput()
         {
             Console.Write("Ange ett nummer (eller 'exit' för att avsluta): ");
-            return Console.ReadLine();
+            return Console.ReadLine()!;
         }
 
         private static bool ÄrExitKommando(string input)
         {
-            return input?.ToLower() == "exit";
+            return input.ToLower() == "exit";
         }
 
         private static int ParsaInputTillNummer(string input)
